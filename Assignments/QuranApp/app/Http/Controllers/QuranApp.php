3c8cc8fd16e36daee6cea53c4b77c58651d22f2e
YousufNaveedKhan <yk03317@gmail.com>
2024-01-12 19:27:16 +0500

    Code Updated

--- Assignments/QuranApp/app/Http/Controllers/QuranApp.php
+++ Assignments/QuranApp/app/Http/Controllers/QuranApp.php
@@ -15,12 +15,10 @@ class QuranApp extends Controller
         return view("Quran", ["myqurandata" => $QuranData["data"]["surahs"]["references"]]);
     }
     function ReadSurah($SurahNumber) {
-        $SurahDetail = Http::get("https://api.alquran.cloud/v1/surah/{$SurahNumber}");
         $AayahDetail = Http::get("http://api.alquran.cloud/v1/surah/{$SurahNumber}/ar.alafasy");
 
         return view("ReadSurah", [
-            "SurahDetails" => $SurahDetail["data"],
-            "AayahDetail" => $AayahDetail["data"]["ayahs"]
+            "AayahDetail" => $AayahDetail["data"]
         ]);
     }
 }
